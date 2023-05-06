c6626bed94ea12a732c5e39c4771901aae18cb67
Anatoly <sindbag@gmail.com>
2023-05-06 15:06:27 +0000

    Updates

--- tg-bot/local_settings.py
+++ tg-bot/local_settings.py
@@ -1,4 +1,9 @@
+import json
+
 BACKEND_API = "http://localhost:8432"
-# WORDS_API_URL_RANDOM = "http://docker-node-mongo:3000/courses"
 LINKEDIN_CLIENT_ID = "78ydgx7xxjf0dz"
 LINKEDIN_CLIENT_SECRET = "DArwc9bJkfMfKvTY"
+
+_creds = json.load(open('credentials.json'))
+LINKEDIN_LOGIN = _creds['login']
+LINKEDIN_PASSWORD = _creds['password']
